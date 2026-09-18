0da5e2f50e53e0edd5083a106fd16753987a27d6
Burlington <perezuburlington@gmail.com>
2026-09-18 18:52:27 +0200

    initial state constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
+import { useState, useRef } from "react";
+import {removeBackground} from "@imgly/background-removal"
+
 export default function BackgroundRemover() {
+
+  const [originalImage, setOriginalImage] = useState(null);
+  const [processedImage, setProcessedImage] = useState(null);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [error, setError] = useState(null);
+  const fileInputRef = useRef(null);
+
+
+  const handleFileSelect = async (file) => {
+    if (!file || !file.type.startsWith("image/")) {
+      setError("Please select a valid image file");
+      return;
+    }
+
+    // clear previous data
+    setError(null);
+    setProcessedImage(null);
+
+    // create File reader
+    const reader = new FileReader();
+  }
+
   return (
     <div className="min-h-screen
       bg-linear-to-br from-fuchsia-950
